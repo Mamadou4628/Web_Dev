20c792b2e6a9d2679dedca537b000d874d33c3ad
Mamadou4628 <158617289+Mamadou4628@users.noreply.github.com>
2024-03-05 23:45:50 -0500

    Initialize app state variable and cached element references

---
+++ Web_Dev/Task_Tracker_App/script.js
@@ -0,0 +1,5 @@
+// script.js 
+// Section 1: TODOs
+// TODO: Register submissions from the user on the form.
+// TODO: Determine the value of the data submitted and add it to a JavaScript array call
+// TODO: Call the render function to update the table with the new tasks.
