d87c10b16cbfba2cd90e64de64ff5ff44b68b249
Mamadou4628 <158617289+Mamadou4628@users.noreply.github.com>
2024-03-22 22:05:49 -0400

    Passed props to Comment component

---
+++ web-projects/social-media-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react';
+import Post from './components/Post';
+function App(){
+return(
+<div>
+<h1>Fakebook!</h1>
+{/* CreatePostForm */}
+{/* Feed */}
+{/* Post */}
+<Post/>
+</div>
+)
+}
+export default App
